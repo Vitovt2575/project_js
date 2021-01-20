4cdfdb9838596cdd03c54845689d46ace09d9534
vitali <smanzer.v@gmail.com>
2021-01-20 12:01:19 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + " - is number");
+
+let incr = 10,
+	decr = 10;
+	
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(incr);
+console.log(decr);
+
+console.log(8 == '8');
