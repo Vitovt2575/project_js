2694d4b5456e4b3cd4dd37bb48d4bfc4df59c84f
vitali <smanzer.v@gmail.com>
2021-03-04 12:19:29 +0300

    first solution

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 "use strict";
 
-console.log('arr' + " - object");
-console.log(4 + " - is number");
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "0");
+console.log(numberOfFilms);
 
-let isChecked = true;
+const personalMovieDB = {
+	count : numberOfFilms,
+	movies : {},
+	actors : {},
+	genres : [],
+	privat : false,
+};
 
-console.log(isChecked);
+let movie = prompt("Один из последних просмотренных фильмов?", "");
+let range = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[movie] = range;
+
+console.log(personalMovieDB.movies);
